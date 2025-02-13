bb0f8a94a1703fbdfbbe610da8cc96fe4c4bfe8d
Sergey Kovalevich <temposta@yandex.ru>
2025-02-13 23:21:56 +0300

    Тесты MpaRepository

---
+++ src/test/java/ru/yandex/practicum/filmorate/repository/user/UserRepositoryImplTest.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.filmorate.repository.user;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserRepositoryImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void checkUser() {
+    }
+
+    @Test
+    void getListUsers() {
+    }
+}
