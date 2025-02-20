7304817a503b71a1e51a693d9e32fbc1a312781b
Arzamastsev <aa@terralink-global.com>
2025-02-21 00:20:52 +0300

    fix: Удалён неиспользуемый импорт

--- src/test/java/ru/yandex/practicum/filmorate/storage/UserDbStorageTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/UserDbStorageTests.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.dal.repository.UserRepository;
 import ru.yandex.practicum.filmorate.dal.storage.user.UserDbStorage;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,7 +21,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Import({UserRowMapper.class, UserRepository.class, UserDbStorage.class})
 class UserDbStorageTests {
 
-	private final UserRepository userRepository;
 	private final UserDbStorage userStorage;
 
 	@Test
